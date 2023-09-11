7c9cb7bc7036f0ce7376bca81ed5b22c08930f8a
Rhudsonl <renerudhson@gmail.com>
2023-09-11 00:45:45 -0300

    Adicionado a função PreStart

--- Program.cs
+++ Program.cs
@@ -36,11 +36,23 @@ namespace Cronometro // Note: actual namespace depends on the project name.
                 System.Environment.Exit(0);
                }
 
-               Start(time * multiplier);
+               PreStart(time * multiplier);
 
         }
 
-        
+        static void PreStart(int time) {
+            Console.Clear();
+            Console.WriteLine("Ready...");
+            Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("Set...");
+            Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("Go...");
+            Thread.Sleep(2500);
+
+            Start(time);
+        }
 
         static void Start(int time) {
 
